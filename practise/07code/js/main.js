b58bf3772e3bfbee4f84db66ab51c8ce959e6512
Lana2333 <39827380+Lana2333@users.noreply.github.com>
2018-10-23 17:13:57 +0800

    Add files via upload

---
+++ practise/07code/js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('#menus').accordion({
+		collapsible: true,
+		active: true,
+		header: 'h3',
+		icons: {
+			"header": "ui-icon-plus",
+			"activeHeader": "ui-icon-minus",
+		},
+	});
+
+});
